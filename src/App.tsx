9a5584251acd6e91b92e1a92a5886d4b6f6305e1
n0-t0 <ray0az0may19@gmail.com>
2024-09-13 00:22:40 +0900

    fix unused2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material';
 import GoalSettingPage from "./pages/GoalSettingPage";
